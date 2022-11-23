07bc4b9391fbfffc3bde271ff1a137c075de2cd3
Yasmin Ferreira <yasmincferreira8@gmail.com>
2022-11-23 15:58:40 -0400

    up

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTecla = document.querySelectorAll('.tecla');
+
+while (0) {
+    listaDeTecla[0].onclick = tocaSomPom;
+}
